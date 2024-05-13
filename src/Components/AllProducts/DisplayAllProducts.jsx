2aa91e49ab2aad102dc79fded7d160524909c05e
abdallahsamir98 <abdallahsamir98>
2024-05-13 20:12:43 +0300

    changes nav

--- src/Components/AllProducts/DisplayAllProducts.jsx
+++ src/Components/AllProducts/DisplayAllProducts.jsx
@@ -17,50 +17,51 @@ const DisplayAllProducts = ({ type }) => {
   return (
     <>
       <Toaster position="top-center" />
-      <div className="cartInfo mt-5 text-center mb-4">
-        <h2>All Products</h2>
-      </div>
+      <div className="cartInfo mt-5   text-center mb-4">
+              <h2>All Products</h2>
+            </div>
       <div className="container">
 
-        {loading && <Loading />}
-        {error && <p>{error} </p>}
+      {loading && <p><Loading/></p>}
+      {error && <p>{error} </p>}
 
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
-          {products?.map((product) => (
-            <div className="col" key={product.id}>
-              <div className="card border-0">
-                <div className="parent position-relative">
-                  <img
-                    src={product.imageCover}
-                    className="card-img-top rounded-1"
-                    alt={product.title}
-                  />
-                  <div className="sibling rounded-1 d-flex justify-content-end align-items-end">
-                    <div className="p-3">
-                      <YourComponent to={`/viewproduct/${product.id}`} product={product} />
-                    </div>
+      <div className="row ">
+        {products?.map((product) => (
+          <div className="col-md-3 " key={product.id}>
+            <div className="card border-0">
+              <div className="parent position-relative">
+                <img
+                  src={product.imageCover}
+                  className="w-100 rounded-1"
+                  alt={product.title}
+                />
+                <div className="sibling rounded-1 d-flex justify-content-end align-items-end  ">
+                  <div className="  h-100 p-3 ">
+                    <YourComponent to={`/viewproduct/${product.id}`} product={product} />
                   </div>
                 </div>
-                <div className="card-body">
-                  <h5 className="card-title">{product.title}</h5>
-                  <p>
-                    {product.priceAfterDiscount ? (
-                      <p className="card-text">
-                        Price:{" "}
-                        <span className="text-decoration-line-through text-danger">
-                          ${product.price}
-                        </span>{" "}
-                        ${product.priceAfterDiscount}
-                      </p>
-                    ) : (
-                      <p className="card-text">Price: ${product.price}</p>
-                    )}
-                  </p>
-                </div>
+              </div>
+              <div className="card-body ">
+                <h5 className="card-title">{product.title}</h5>
+                <p>
+                  {product.priceAfterDiscount ? (
+                    <p className="card-text ">
+                      Price:{" "}
+                      <span className="text-decoration-line-through text-danger ">
+                        {" "}
+                        ${product.price}
+                      </span>{" "}
+                      ${product.priceAfterDiscount}
+                    </p>
+                  ) : (
+                    <p className="card-text ">Price:${product.price} </p>
+                  )}
+                </p>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
+      </div>
       </div>
     </>
   );
--- src/Components/AllProducts/ProdcuctsCard.jsx
+++ src/Components/AllProducts/ProdcuctsCard.jsx
@@ -1,11 +1,18 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./Products.css";
 import React from "react";
+import { CiHeart } from "react-icons/ci";
+import { FaEye } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { FaCartPlus } from "react-icons/fa";
+import ProductLinksPopover from "./ProductLinksPopover";
+import YourComponent from "./ProductLinksPopover";
+import { IoEyeOutline } from "react-icons/io5";
+import "./Products.css"
 import { Toaster } from "react-hot-toast";
 import Loading from "../Loading/Loading";
-
 const ProductsCard = ({ type }) => {
-  const { products, loading } = useSelector((state) => state.products);
+  const { products ,loading} = useSelector((state) => state.products);
   const filteredProducts = products
     ? products.filter(
         (product) =>
@@ -16,40 +23,66 @@ const ProductsCard = ({ type }) => {
       )
     : [];
 
+  
+    
   return (
     <>
-      <Toaster position="top-center" />
-      {loading && <div><Loading/></div>}
-
-      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
-        {filteredProducts.slice(0, 8).map((product) => (
-          <div className="col" key={product.id}>
-            <div className="card border-0">
-              <img
-                src={product.imageCover}
-                className="card-img-top rounded-1"
-                alt={product.title}
-              />
-              <div className="card-body">
-                <h5 className="card-title">{product.title}</h5>
-                <div>
-                  {product.priceAfterDiscount ? (
-                    <p className="card-text">
-                      Price:{" "}
-                      <span className="text-decoration-line-through text-danger">
-                        ${product.price}
-                      </span>{" "}
-                      ${product.priceAfterDiscount}
-                    </p>
-                  ) : (
-                    <p className="card-text">Price: ${product.price}</p>
-                  )}
+<Toaster position="top-center" />
+{loading && <div><Loading/></div>}
+<div className="row">
+{products &&
+        products.length > 0 &&
+        filteredProducts.slice(0, 8).map((product) => (
+          <div className="col-md-3   mt-2" key={product.id}>
+            {product.category.name === type &&
+              product.title &&
+              product.imageCover &&
+              product.price && (
+                <div className="">
+                  <div className="card border-0">
+                    <div className="parent position-relative">
+                      <img
+                        src={product.imageCover}
+                        className="w-100 rounded-1"
+                        alt={product.title}
+                      />
+                      <div className="sibling rounded-1 d-flex justify-content-end align-items-end  ">
+                        <div className="  h-100 p-3 ">
+                           
+<YourComponent  to={`/viewproduct/${product.id}`} product={product}  />
+                         
+    
+                     
+                        </div>
+                      </div>
+                    </div>
+                    <div className="card-body ">
+                      <h5 className="card-title">{product.title}</h5>
+                      <div>
+                        {product.priceAfterDiscount ? (
+                          <p className="card-text ">
+                            Price:{" "}
+                            <span className="text-decoration-line-through text-danger ">
+                              {" "}
+                              ${product.price}
+                            </span>{" "}
+                            ${product.priceAfterDiscount}
+                          </p>
+                        ) : (
+                          <p className="card-text ">Price:${product.price} </p>
+                        )}
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
+              )}
+              
           </div>
+          
         ))}
-      </div>
+</div>
+    
+      
     </>
   );
 };
